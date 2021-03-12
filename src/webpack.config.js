92d0df59fcb2b63b5b64f8c8e31b1bc4ff2ba02a
Jake Gealer <jake@gealer.email>
2021-03-12 04:48:46 +0000

    give up with that space saving attempt

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -99,7 +99,7 @@ module.exports = (source, dest, dev) => ({
             fs: require.resolve('browserify-fs'),
             buffer: require.resolve('buffer/'),
             util: require.resolve('util/'),
-            stream: require.resolve('readable-stream'),
+            stream: require.resolve('stream-browserify'),
             events: require.resolve('events/'),
             constants: require.resolve('constants-browserify'),
         },
