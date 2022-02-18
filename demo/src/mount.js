ef164a59eb9b3961821265dd859272337ff3bb53
MattIPv4 <me@mattcowley.co.uk>
2022-02-18 17:47:41 +0000

    Upgrade to Vue 3

--- demo/src/mount.js
+++ demo/src/mount.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 DigitalOcean
+Copyright 2022 DigitalOcean
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,29 +14,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// This is just a pure DOM implementation of a very simple counter.
-// We are not trying to test Vue or React here, we have plenty of tools to do that.
-// Rather, we are making sure the CLI builds it and handles things like hot reloading properly.
+// This aims to test the fundamentals of everything this build package is used for.
+// Loading Vue, SCSS, normal JS, all through Webpack.
+
+import { createApp } from 'vue';
+import App from './templates/app.vue';
 
 // Imports the CSS.
 import './scss/style';
-
-// Test importing from a very far away path (webpack config testing).
-import incrementCounter from './in/a/path/far/far/away/increment';
-
-// Get the counter element.
-const counterElement = document.getElementById('counter');
-
-// Adjust the counter in the window.
-window.addOne = () => {
-    // Update the counter.
-    counterElement.innerText = String(incrementCounter());
-};
-
-// Make sure other elements aren't in global scope.
-if (window.counter === 0 || window.counterElement) throw new Error('Parts of mount should not be in global scope.');
 
 // Try out dynamic stuff
 window.loadDynamicAlert = async () => {
     await import('./dynamic/alert');
 };
+
+createApp(App).mount('#app');
