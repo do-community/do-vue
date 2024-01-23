676f84072143b05cad765242b4b5a31c301d4a6e
Matt Cowley <me@mattcowley.co.uk>
2024-01-23 02:12:25 +0000

    Fix missing space in ExternalLink (#79)

--- src/templates/external_link.vue
+++ src/templates/external_link.vue
@@ -1,5 +1,5 @@
 <!--
-Copyright 2022 DigitalOcean
+Copyright 2024 DigitalOcean
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ limitations under the License.
 <template>
     <span>
         <a :href="link" target="_blank" rel="noreferrer" v-text="text"></a>
+        {{ " " }}
         <i class="fas fa-external-link-alt"></i>
     </span>
 </template>
